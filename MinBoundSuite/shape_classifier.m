b4c21b3a894b8e9e886a1a8bd2fcd5e1548e2a5d
abbet <abbet.christian@gmail.com>
2017-05-19 16:51:59 +0200

    robot detection, shape detection better, color clustering

--- MinBoundSuite/shape_classifier.m
+++ MinBoundSuite/shape_classifier.m
@@ -4,15 +4,15 @@ function [ p ] = shape_classifier( region )
 
 
 image = region.Image;
-area_gt = region.Area;
-comp_gt = region.Perimeter^2/region.Area;
+area_gt = region.FilledArea;
 
 % 1. Take bounding box fit for first comparison
 % Take only contour, no need to feed with internal points
-BW = edge(image,'Sobel');
+% BW = edge(image,'Sobel');
 % Look for coordinates of contour
-ids = find(BW == 1);
-[X,Y] = ind2sub(size(BW), ids);
+
+ids = find(image == 1);
+[X,Y] = ind2sub(size(image), ids);
 
 % 1. Fit circle
 [~, radius] = minboundcircle(X,Y);
@@ -23,21 +23,15 @@ r_tri_est = [trix(1:2)-trix(2:3), triy(1:2)-triy(2:3)];
 u = [r_tri_est(1,:), 0]; v = [r_tri_est(2,:), 0]; % Extract sides
 sin_t = norm(cross(u,v))/(norm(u)*norm(v)); % Extract sin(x)
 r_tri_est = 0.5*sin_t*prod(sqrt(sum(abs(r_tri_est).^2,2))); % Get Area
-% 3. Fit rectangle
+% 3. Fit squre ! Take care of ratio
 [rectx, recty, ~, ~] = minboundrect(X, Y, 'a');
 r_squ_est = [rectx(1:2)-rectx(2:3), recty(1:2)-recty(2:3)]; % Extract sides
-r_squ_est = prod(sqrt(sum(abs(r_squ_est).^2,2))); % Get Area
+r_squ_edge = sqrt(sum(abs(r_squ_est).^2,2));
+r_squ_est = prod(sqrt(sum(abs(r_squ_est).^2,2))) * max(r_squ_edge)/min(r_squ_edge); % Get Area
 
 % Return ratio with original size
 p_bb = area_gt*1./([r_circle_est, r_squ_est, r_tri_est]);
-
-%2 . Take compacity for nex feature
-p_circle = normpdf(comp_gt, 4*pi, 1.5);
-p_square = normpdf(comp_gt, 16, 2);
-p_tri = normpdf(comp_gt, (9*4)/sqrt(3), 3);
-p_co = [p_circle, p_square, p_tri];
-
-p = p_bb.*p_co;
+p = p_bb;
 
 end
 
---
+++ robot_fit.m
@@ -0,0 +1,56 @@
+function [ region_rob ] = robot_fit( im_original, config )
+%ROBOT_FIT Summary of this function goes here
+%   Detailed explanation goes here
+
+% Main variables
+size_image = size(im_original, 1)*size(im_original, 2);
+
+% 1. Go to HSV ref to threshold value (black)
+hsv_im = rgb2hsv(im_original);
+thresh = hsv_im(:,:,3) < config.black_v_thresh;
+
+% 2. Get matching regions and look for bigest (according also to limit
+% size)
+region_rob = regionprops(thresh, 'Area', 'Image', 'Orientation', 'Centroid');
+
+areas = [];
+for i = length(region_rob)-1:-1:1
+    if region_rob(i).Area/size_image < config.size_min_thresh ...
+        || region_rob(i).Area/size_image > config.size_max_thresh
+        region_rob(i) = [];
+        continue
+    end
+    
+    areas = [region_rob(i).Area, areas];
+end
+
+if length(areas) == 0
+    region_rob = [];
+    return
+end
+
+[~, I] = max(areas);
+region_rob = region_rob(I);
+
+% 3. Look for angle of arrrow (according to x axis)
+% Project point on main axis to look for direction. Region of arrow head
+% will be more dense
+ids = find(region_rob.Image == 1);
+[row, col] = ind2sub(size(region_rob.Image), ids);
+thetha_ref2 = deg2rad(region_rob.Orientation) + pi/2;
+
+% 4. Check interval [-pi, pi] is correct
+% Center image according to image center .... Could be an issue
+% (considering centroid might be better)
+X = row - size(region_rob.Image,1)/2; Y = col - size(region_rob.Image,2)/2;
+base = [cos(thetha_ref2); sin(thetha_ref2)];
+v = dot([X,Y]',base*ones(1, length(X))); % Project point on axis
+
+% Check if need to inverse dirrection (arrow head pointing other direction)
+if sum(v < 0) > sum(v > 0)
+    region_rob.Orientation = radtodeg(pi) + region_rob.Orientation;
+end
+
+
+end
+
