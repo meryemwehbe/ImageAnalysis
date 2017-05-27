bf228ca87f6d71e58dfc10e48be14f76c0b3490a
Patryk Oleniuk <patryk.oleniuk@gmail.com>
2017-05-27 18:16:31 +0200

    Minor changes

--- cal_angle.m
+++ cal_angle.m
@@ -2,8 +2,8 @@ function [ angle ] = cal_angle( path )
 %CAL_ANGLE Summary of this function goes here
 %   Calculates angle from reference vector [0 1 0] - > z coordinate only
 %   there for calculation purposes
-u = [-1 0 0];
-v = [ (path(2,1)-path(1,1))  path(2,2)-path(1,2) 0];
+u = [1 0 0];
+v = [ (path(1,1)-path(2,1))  path(1,2)-path(2,2) 0];
 
 angle = atan2d(norm(cross(u,v)),dot(u,v));
 
--- robot_controller_func/turn_deg.m
+++ robot_controller_func/turn_deg.m
@@ -1,13 +1,13 @@
 function [ ] = turn_deg( angle , motor_l, motor_r)
 % turn the robot into some angle 
 
-if(abs(angle) < 15) % if angle is small(<15deg), move slower fro smaller error
+if(abs(angle) < 15) % if angle is small(<15deg), move slower for smaller error
     motor_l.Speed=-15*sign(angle);
     motor_r.Speed=15*sign(angle);
     start(motor_l);start(motor_r);
-    pause(abs(angle/20)) % the 20 constant is experimental but proven
+    pause(abs(angle/10)) % the 10 constant is experimental but proven
     stop(motor_r);stop(motor_l);
-elseif(abs(angle) < 90) % if angle is middle, move middle slow fro smaller error
+elseif(abs(angle) < 90) % if angle is middle, move middle slow for smaller error
     motor_l.Speed=-50*sign(angle);
     motor_r.Speed=50*sign(angle);
     start(motor_l);start(motor_r);
